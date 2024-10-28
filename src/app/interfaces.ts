1af5658923ff0936a9501a0d99c85cba82558fb9
Andrey <karpovich.andrey@gmail.com>
2024-10-28 18:49:49 +0200

    MAPBOX. Interfaces, styles

--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -4,8 +4,42 @@ export interface INDVIproperties {
     evi?: number,
 }
 
+export interface IMapboxLocation {
+    name: string,
+    label: string,
+    initialZoom: number,
+    coords: number[],
+    layers?: string[]
+
+}
+export interface IMapboxMarker {
+    name: string,
+    coords: number[],
+    location?: string,
+}
+
+interface IGeojsonData {
+    type: "FeatureCollection",
+    features: any[]
+}
+
+export interface IMapboxGeojson {
+    type: 'geojson',
+    data: IGeojsonData
+}
+
+//------- CONSTANTS --------- //
+
 export const blankINDVIproperties = {
     id: '',
     ndvi: null,
     evi: null,
+}
+
+export const BlankGeojson: IMapboxGeojson = {
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: []
+    }
 }
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,8 +1,33 @@
 // ---------------------   INTERACTIVITY, HANDLERS todo feature versions  ----------------//
 
-const COLOR_SHADES = {
+export const COLOR_SHADES = {
+    // lightest first
+    blue: [
+        "#70eafa",
+        "#62d3ec",
+        "#57bddd",
+        "#4ea7cc",
+        "#4792bb",
+        "#427ca8",
+        "#3c6894",
+        "#375480",
+        "#30416b",
+        "#292f56"
+    ],
     // darkest first
-    green: [ '#33691e', '#4f8338', '#6b9d52', '#86b76b', '#a2d185', '#beeb9f'],
+    violet: [
+        '#36175e',
+        '#40216a',
+        '#4a2b75',
+        '#553581',
+        '#5f3f8e',
+        '#6a4a9a',
+        '#7454a6',
+        '#7f5fb3',
+        '#8a6ac0',
+        '#9575cd'
+    ],
+    green: ['#33691e', '#4f8338', '#6b9d52', '#86b76b', '#a2d185', '#beeb9f'],
     orange: ['#D23600', '#DF5600', '#ED7600', '#FA9600'],
 }
 
@@ -51,18 +76,18 @@ export function setInitialStyles(feature, isDrawn?: boolean) {
 //green palette
 export function getMoistureColor(d): string {
     const g: string[] = COLOR_SHADES.green;
-    return  d > 20 ? g[0] :
-            d > 0 ? g[1] :
+    return d > 20 ? g[0] :
+        d > 0 ? g[1] :
             d > -20 ? g[2] :
-            d > -40 ? g[3] :
-            d > -60 ? g[4] :
-            g[5] || '';
+                d > -40 ? g[3] :
+                    d > -60 ? g[4] :
+                        g[5] || '';
 }
 //orange palette
 export function getNDVIColor(d): string {
     const o: string[] = COLOR_SHADES.orange;
-    return  d > 0.5 ? o[0] :
-            d > 0 ? o[1] :
+    return d > 0.5 ? o[0] :
+        d > 0 ? o[1] :
             d > -0.5 ? o[2] :
-            o[3] || '';
+                o[3] || '';
 }
