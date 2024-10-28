feae885d722ec3f6496866fb4264a0b8a5a34937
Andrey <karpovich.andrey@gmail.com>
2024-10-28 18:49:24 +0200

    MAPBOX.  Corrs for app and leaflet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { LeafletComponent } from './leaflet/leaflet.component';
+import { MapboxGlComponent } from './mapbox-gl/mapbox-gl.component';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LeafletComponent],
+  imports: [NgForOf, LeafletComponent, MapboxGlComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  links = [{
+    link: 'leaflet',
+    label: 'Leaflet (S.Arabia)'
+  },
+  {
+    link: 'mapboxgl',
+    label: 'Mapbox-GL'
+  },
+]
 }
