f2fba24c6923a8c70c9c6a1273a44679491dc1cb
Andrey <karpovich.andrey@gmail.com>
2024-10-28 18:50:07 +0200

    MAPBOX.  Configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,35 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, Routes } from '@angular/router';
+import { LeafletComponent } from './leaflet/leaflet.component';
+import { MapboxGlComponent } from './mapbox-gl/mapbox-gl.component';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'mapboxgl',
+    pathMatch: 'full',
+  },
+  {
+    path: 'mapboxgl',
+    component: MapboxGlComponent
+  },
+  {
+    path: 'leaflet',
+    component: LeafletComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'leaflet',
+    pathMatch: 'full'
+  }
+];
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
+    provideRouter(routes),
+    importProvidersFrom(NgxMapboxGLModule)
+  ]
 };
+
